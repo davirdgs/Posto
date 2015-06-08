63b71b7f53c167f45b59d55e16896ccb4e8280a2
Davi Rodrigues <davirdgs@MacBook-Pro-de-Davi.local>
2015-06-08 14:11:24 -0300

    menu

---
+++ posto/posto/Controllers/MenuViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MenuViewController.swift
+//  posto
+//
+//  Created by Davi Rodrigues on 08/06/15.
+//  Copyright (c) 2015 Davi Rodrigues. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+}
