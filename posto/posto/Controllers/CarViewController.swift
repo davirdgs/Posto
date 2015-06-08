b594076cfb343f392a5850a0c5232c96d39669c2
Davi Rodrigues <davirdgs@MacBook-Pro-de-Davi.local>
2015-06-08 16:29:27 -0300

    fim do primeiro dia

--- posto/posto/Controllers/CarViewController.swift
+++ posto/posto/Controllers/CarViewController.swift
@@ -8,7 +8,19 @@
 
 import UIKit
 
-class CarViewController: UIViewController {
+class CarViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate
+{
+    @IBOutlet weak var modelPicker: UIPickerView!
+    @IBOutlet weak var modelLabel: UILabel!
+    let pickerData = ["Mozzarella","Gorgonzola","Provolone","Brie","Maytag Blue","Sharp Cheddar","Monterrey Jack","Stilton","Gouda","Goat Cheese", "Asiago"]
+    
+    
+    
+    @IBOutlet weak var kmTextField: UITextField!
+    @IBOutlet weak var oilTextField: UITextField!
+    @IBOutlet weak var reviewTextField: UITextField!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,5 +43,34 @@ class CarViewController: UIViewController {
     // Pass the selected object to the new view controller.
     }
     */
+    
+    //MARK: - Delegates and data sources
+    //MARK: Data Sources
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    //MARK: Delegates
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return pickerData[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //myLabel.text = pickerData[row]
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        textField.clearsOnBeginEditing = false
+        return true
+    }
+    
+    @IBAction func handleSingleTap(sender: AnyObject) {
+        self.view.endEditing(true)
+    }
+    
     
 }
