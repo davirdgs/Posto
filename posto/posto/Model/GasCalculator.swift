a9d03e3225cec8f34c07cb9c2217ad184321d7f2
Matheus Santos Lopes <matheus.fcc.faj@gmail.com>
2015-06-09 12:19:16 -0300

    novo commit

---
+++ posto/posto/Model/GasCalculator.swift
@@ -0,0 +1,13 @@
+//
+//  GasCalculator.swift
+//  posto
+//
+//  Created by Matheus Santos Lopes on 09/06/15.
+//  Copyright (c) 2015 Davi Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class GasCalculator: NSObject {
+   
+}
